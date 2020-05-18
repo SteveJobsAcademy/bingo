1f7638737f0bc0aa950dd1fc4d9ef85ff9c1d113
Carlo Leonardi <carloleonardi83@gmail.com>
2020-05-18 10:40:48 +0200

    first commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+package bingo;
+
+public class Main {
+    public static void main(String args[]) {
+        Bingo b = new Bingo();
+        Player p = b.addPlayer("Carlo");
+        System.out.println(p);
+        b.addPlayer("Giovanni");
+        b.addPlayer("Giacomo");
+        b.addPlayer("Aldo");
+        b.addPlayer("Mario");
+        while(b.checkWin() == false) {
+            b.extract();
+        }
+        System.out.println(b);
+    }
+}
